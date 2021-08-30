49d5b1aaf8a451241ad8281a9cfedf80877cfa7f
givemesomecoffee <givemesomecoffee@yandex.ru>
2021-08-31 00:44:17 +0300

    added TODO list

---
+++ app/src/main/java/ru/givemesomecoffee/tasker/TODO.kt
@@ -0,0 +1,22 @@
+package ru.givemesomecoffee.tasker
+
+//UI
+//TODO: add cardView for tasks list
+//TODO: remove ActionBar
+//TODO: rename SingleTask to EditTask, create SingleTaskView
+
+//CODE
+//TODO: remove allow mainThread in room database
+//TODO: add viewModels
+//TODO: try to decompose app -> data, core, features -- optional
+//TODO: add repository
+//TODO: remove fallbackDestructive in room and add migration
+
+//FUNCTIONALITY
+//TODO: add date created, date to finish in Tasks
+//TODO: implement google authentication
+//TODO: implement backup from google drive
+//TODO: add notifiers for tasks
+//TODO: add intent for reminders
+//TODO: add filters(tags?) for tasks
+//TODO: add paging for tasks list
